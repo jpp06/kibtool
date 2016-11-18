8313bed769e1d78c48bb19d757eb274957c7c902
jpparis-orange <jeanpierre.paris@orange.com>
2016-11-18 15:54:39 +0100

    integration test

---
+++ test/integration/__init__.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+import time
+import os
+import shutil
+import tempfile
+import random
+import string
+from datetime import timedelta, datetime, date
+
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import ConnectionError
+
+from unittest import SkipTest, TestCase
+
+client = None
+
+
+host, port = os.environ.get('TEST_ES_SERVER', 'localhost:9200').split(':')
+port = int(port) if port else 9200
+
+def get_client():
+  global client
+  if client is not None:
+    return client
+
+  client = Elasticsearch([os.environ.get('TEST_ES_SERVER', {})], timeout=300)
+
+  # wait for yellow status
+  for _ in range(100):
+    time.sleep(.1)
+    try:
+      client.cluster.health(wait_for_status='yellow')
+      return client
+    except ConnectionError:
+      continue
+  else:
+    # timeout
+    raise SkipTest("Elasticsearch failed to start.")
+
+def setup():
+  get_client()
+
+class Args(dict):
+    def __getattr__(self, att_name):
+        return self.get(att_name, None)
+
+class KibtoolTestCase(TestCase):
+  def setUp(self):
+    super(KibtoolTestCase, self).setUp()
+    self.client = get_client()
+
+    args = {
+      "prefix": "kibtool-"
+    }
+    args['host'], args['port'] = host, port
+    self.args = args
+  def tearDown(self):
+    self.client.indices.delete(index=self.args["prefix"] + '*')
+
+  def create_indices(self, count, unit=None):
+    self.create_index(self.args['prefix'] + datetime(d.year, d.month, 1).strftime(format), wait_for_yellow=False)
+    self.client.cluster.health(wait_for_status='yellow')
+
+  def create_index(self, name, shards=1, wait_for_yellow=True):
+    self.client.indices.create(
+      index=name,
+      body={'settings': {'number_of_shards': shards, 'number_of_replicas': 0}}
+    )
+    if wait_for_yellow:
+      self.client.cluster.health(wait_for_status='yellow')
+
+  def add_docs(self, idx):
+    for i in ["1", "2", "3"]:
+      self.client.create(
+        index=idx, doc_type='log', id=i,
+        body={"doc" + i :'TEST DOCUMENT'},
+      )
+      # This should force each doc to be in its own segment.
+      self.client.indices.flush(index=idx, force=True)
+
+  def close_index(self, name):
+    self.client.indices.close(index=name)
+
+  def write_config(self, fname, data):
+    with open(fname, 'w') as f:
+      f.write(data)
---
+++ test/integration/test_none.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+import os
+from nose.tools import *
+from io import StringIO
+from unittest.mock import patch
+
+import elasticsearch
+from elasticsearch import exceptions
+import kibtool
+
+from . import KibtoolTestCase
+
+host, port = os.environ.get('TEST_ES_SERVER', 'localhost:9200').split(':')
+port = int(port) if port else 9200
+
+class TestNone(KibtoolTestCase):
+  def test_none(self):
+    self.assertEquals(
+      [],
+      list(self.client.indices.get(self.args["prefix"] + "*").keys())
+    )
+
+    with patch('sys.stdout', new=StringIO()) as fake_out, patch('sys.stderr', new=StringIO()) as fake_err:
+      l_kibtool = kibtool.KibTool(["./test_kibtool"])
+      l_kibtool.execute()
+      self.assertEquals(fake_out.getvalue().strip(), "")
+      self.assertEquals(fake_err.getvalue().strip(), "")
+
+    self.assertEquals(
+      [],
+      list(self.client.indices.get(self.args["prefix"] + "*").keys())
+    )
+
+  def test_no_index_no_read(self):
+    self.assertEquals(
+      [],
+      list(self.client.indices.get(self.args["prefix"] + "*").keys())
+    )
+
+    with patch('sys.stdout', new=StringIO()) as fake_out, patch('sys.stderr', new=StringIO()) as fake_err:
+      l_kibtool = kibtool.KibTool(["./test_kibtool",
+                                   "--kibfrom", self.args["prefix"] + "zzz",
+                                   "--print"])
+      l_kibtool.execute()
+      self.assertEquals(fake_out.getvalue().strip(), "")
+      self.assertEquals(fake_err.getvalue().strip(), "")
+
+
+    self.assertEquals(
+      [],
+      list(self.client.indices.get(self.args["prefix"] + "*").keys())
+    )
+
+  def test_no_index_dashid(self):
+    self.assertEquals(
+      [],
+      list(self.client.indices.get(self.args["prefix"] + "*").keys())
+    )
+
+    with patch('sys.stdout', new=StringIO()) as fake_out, patch('sys.stderr', new=StringIO()) as fake_err:
+      l_kibtool = kibtool.KibTool(["./test_kibtool",
+                                   "--kibfrom", self.args["prefix"] + "zzz",
+                                   "--dashid", "zzz",
+                                   "--print"])
+      l_kibtool.execute()
+      self.assertEquals(fake_out.getvalue().strip(), "kibtool-zzz/dashboard/zzz")
+      self.assertEquals(fake_err.getvalue().strip(), "")
+
+    self.assertEquals(
+      [],
+      list(self.client.indices.get(self.args["prefix"] + "*").keys())
+    )
+  @raises(exceptions.NotFoundError)
+  def test_no_index_dash(self):
+    self.assertEquals(
+      [],
+      list(self.client.indices.get(self.args["prefix"] + "*").keys())
+    )
+
+    with patch('sys.stdout', new=StringIO()) as fake_out, patch('sys.stderr', new=StringIO()) as fake_err:
+      l_kibtool = kibtool.KibTool(["./test_kibtool",
+                                   "--kibfrom", self.args["prefix"] + "zzz",
+                                   "--dash", "zzz",
+                                   "--print"])
+      l_kibtool.execute()
+      self.assertEquals(fake_out.getvalue().strip(), "")
+      self.assertEquals(fake_err.getvalue().strip(), "")
+
+    self.assertEquals(
+      [],
+      list(self.client.indices.get(self.args["prefix"] + "*").keys())
+    )
