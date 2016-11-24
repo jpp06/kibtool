ed150e0b69026be835a13c6753c00b0c16b89d71
Jean-Pierre Paris <jeanpierre.paris@orange.com>
2016-11-24 21:55:11 +0100

    Dashboard count (#29)
    * wrong method names
    
    * fixes #26

--- test/integration/test_dashboardcount.py
+++ test/integration/test_dashboardcount.py
@@ -18,13 +18,13 @@ logging.getLogger("urllib3").setLevel(logging.ERROR)
 host, port = os.environ.get('TEST_ES_SERVER', 'localhost:9200').split(':')
 port = int(port) if port else 9200
 
-class TestCopy(KibtoolTestCase):
+class TestDashboardCount(KibtoolTestCase):
   def test_copy_dashid(self):
     (l_srcName, l_dstName) = self.create_indices()
 
     with patch('sys.stdout', new=StringIO()) as fake_out, patch('sys.stderr', new=StringIO()) as fake_err:
       l_kibtool = kibtool.KibTool(["./test_kibtool", "--kibfrom", l_srcName, "--kibto", l_dstName,
-                                   "--dashid", "dashboard-1",
+                                   "--dashid", "dashboard-1", "--count", "0",
                                    "--copy"])
       l_kibtool.execute()
       self.assertEquals(fake_out.getvalue().strip(), "")
@@ -43,62 +43,50 @@ class TestCopy(KibtoolTestCase):
 
     with patch('sys.stdout', new=StringIO()) as fake_out, patch('sys.stderr', new=StringIO()) as fake_err:
       l_kibtool = kibtool.KibTool(["./test_kibtool", "--kibfrom", l_srcName, "--kibto", l_dstName,
-                                   "--dash", "dashboard 1",
+                                   "--dash", "dashboard 1", "--count", "0",
                                    "--copy"])
-      l_kibtool.execute()
+      with self.assertRaises(SystemExit) as w_se:
+        l_kibtool.execute()
+        self.assertEqual(w_se.exception, "Error")
       self.assertEquals(fake_out.getvalue().strip(), "")
-      self.assertEquals(fake_err.getvalue().strip(), "")
+      self.assertEquals(fake_err.getvalue().strip(), "*** Please use a greater --count (1) to select all dashboards")
 
     l_src = self.client.get(index=l_srcName, doc_type="dashboard", id="dashboard-1")
     l_srcIdx = l_src.pop("_index")
-    l_dst = self.client.get(index=l_dstName, doc_type="dashboard", id="dashboard-1")
-    l_dstIdx = l_dst.pop("_index")
     self.assertEquals(l_srcIdx, l_srcName)
-    self.assertEquals(l_dstIdx, l_dstName)
-    self.assertEquals(l_src, l_dst)
+    l_dst = self.client.search(index=l_dstName, doc_type="dashboard", body={
+      "query": {
+        "match_all": {
+        }
+      }
+    })
+    self.assertEquals(l_dst["hits"]["total"], 0)
 
-  def test_copy_dashid_depend(self):
+  def test_copy_dash2(self):
     (l_srcName, l_dstName) = self.create_indices()
 
     with patch('sys.stdout', new=StringIO()) as fake_out, patch('sys.stderr', new=StringIO()) as fake_err:
       l_kibtool = kibtool.KibTool(["./test_kibtool", "--kibfrom", l_srcName, "--kibto", l_dstName,
-                                   "--dashid", "dashboard-1",
-                                   "--depend", "--copy"])
-      l_kibtool.execute()
+                                   "--dash", "dashboard", "--count", "1",
+                                   "--copy"])
+      with self.assertRaises(SystemExit) as w_se:
+        l_kibtool.execute()
+        self.assertEqual(w_se.exception, "Error")
       self.assertEquals(fake_out.getvalue().strip(), "")
-      self.assertEquals(fake_err.getvalue().strip(), "")
+      self.assertEquals(fake_err.getvalue().strip(), "*** Please use a greater --count (2) to select all dashboards")
 
     l_src = self.client.get(index=l_srcName, doc_type="dashboard", id="dashboard-1")
     l_srcIdx = l_src.pop("_index")
-    l_dst = self.client.get(index=l_dstName, doc_type="dashboard", id="dashboard-1")
-    l_dstIdx = l_dst.pop("_index")
     self.assertEquals(l_srcIdx, l_srcName)
-    self.assertEquals(l_dstIdx, l_dstName)
-    self.assertEquals(l_src, l_dst)
-
-    l_src = self.client.get(index=l_srcName, doc_type="visualization", id="visualization-1")
+    l_src = self.client.get(index=l_srcName, doc_type="dashboard", id="dashboard-8")
     l_srcIdx = l_src.pop("_index")
-    l_dst = self.client.get(index=l_dstName, doc_type="visualization", id="visualization-1")
-    l_dstIdx = l_dst.pop("_index")
     self.assertEquals(l_srcIdx, l_srcName)
-    self.assertEquals(l_dstIdx, l_dstName)
-    self.assertEquals(l_src, l_dst)
-
-    l_src = self.client.get(index=l_srcName, doc_type="search", id="search-1")
-    l_srcIdx = l_src.pop("_index")
-    l_dst = self.client.get(index=l_dstName, doc_type="search", id="search-1")
-    l_dstIdx = l_dst.pop("_index")
-    self.assertEquals(l_srcIdx, l_srcName)
-    self.assertEquals(l_dstIdx, l_dstName)
-    self.assertEquals(l_src, l_dst)
-
-    l_src = self.client.get(index=l_srcName, doc_type="index-pattern", id="index-pattern-1")
-    l_srcIdx = l_src.pop("_index")
-    l_dst = self.client.get(index=l_dstName, doc_type="index-pattern", id="index-pattern-1")
-    l_dstIdx = l_dst.pop("_index")
-    self.assertEquals(l_srcIdx, l_srcName)
-    self.assertEquals(l_dstIdx, l_dstName)
-    self.assertEquals(l_src, l_dst)
-
+    l_dst = self.client.search(index=l_dstName, doc_type="dashboard", body={
+      "query": {
+        "match_all": {
+        }
+      }
+    })
+    self.assertEquals(l_dst["hits"]["total"], 0)
 
 # ./test_kibtool --kibfrom kibtool-src --kibto kibtool-dst --dashid dashboard-1 --depend --copy
