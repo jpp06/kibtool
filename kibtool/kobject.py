e011fd232d523f2b0f33f0b16cdb1ca6e62f1dfe
Jean-Pierre Paris <jeanpierre.paris@orange.com>
2017-01-19 20:19:17 +0100

    unused toBulk

--- kibtool/kobject.py
+++ kibtool/kobject.py
@@ -62,15 +62,6 @@ class KObject(object):
     except exceptions.NotFoundError as e:
       return
 
-  def toBulk(self):
-    self.readFromEs()
-    if not self.m_json:
-      print("*** Can not get '%s' object from '%s'" % (self, self.m_index), file=sys.stderr)
-      sys.exit(3)
-    return [
-      { "index" : { "_type" : self.m_type, "_id" : self.m_id } },
-      self.m_json["_source"]
-    ]
   def toKibanaImpExp(self):
     self.readFromEs()
     if not self.m_json:
