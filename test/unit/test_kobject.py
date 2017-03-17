f78a6ea18b73fd4d5331625444b237a4292b82f1
Jean-Pierre Paris <jeanpierre.paris@orange.com>
2017-03-17 19:06:53 +0100

    fai intg tests
    fixes #37, fixes #51

--- test/unit/test_kobject.py
+++ test/unit/test_kobject.py
@@ -108,7 +108,7 @@ class TestDashboard(TestCase):
     l_obj.getDepend()
 
 
-class TestQuery(TestCase):
+class TestFai(TestCase):
   def testString(self):
     l_obj = kibtool.KObject(None, "an_index", "a_type", "an_id")
     l_fai = l_obj.getFieldsFromQueryString("*")
@@ -126,7 +126,7 @@ class TestQuery(TestCase):
     l_fai = l_obj.getFieldsFromQueryString("NOT F:V")
     self.assertEquals(l_fai, set([ ("F", "field") ]))
     l_fai = l_obj.getFieldsFromQueryString("(F1:this OR F2:this) AND (F1:that OR F2:that)")
-    l_fai = l_obj.getFieldsFromQueryString("F1:V1 AND F2:V2")
+    self.assertEquals(l_fai, set([ ("F1", "field"), ("F2", "field") ]))
 
   def testDict(self):
     l_obj = kibtool.KObject(None, "an_index", "a_type", "an_id")
@@ -134,5 +134,12 @@ class TestQuery(TestCase):
     self.assertEquals(l_fai, set())
     l_fai = l_obj.getFieldsFromQuery({"match":{"F":"V"}})
     self.assertEquals(l_fai, set([ ("F", "field") ]))
+
+  def testExpr(self):
+    l_obj = kibtool.KObject(None, "an_index", "a_type", "an_id")
+    l_fai = l_obj.getFaiFromExpression("")
+    self.assertEquals(l_fai, set())
+    l_fai = l_obj.getFaiFromExpression(".es(index='I1', timefield='F1',kibana='true').label( 'label0') .es(index=' I2', timefield='F2',kibana='true').label('label3')")
+    self.assertEquals(l_fai, set([ ("F1", "field"), ("F2", "field"), ("I1", "index"), ("I2", "index") ]))
 
 #  {“query”: { “match”: { “_all”: “meaning” } } }
